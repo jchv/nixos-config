3c1a6ff95eec85ba3251a6607663cde5903bb6b8
John Chadwick <john@jchw.io>
2024-01-28 04:55:09 -0500

    nvim improvements

--- modules/programs/vim/config.nix
+++ modules/programs/vim/config.nix
@@ -3,6 +3,14 @@
     dracula.enable = true;
   };
   plugins = {
+    auto-session = {
+      enable = true;
+      extraOptions = {
+        pre_save_cmds = ["lua close_file_tree()"];
+        post_save_cmds = ["lua open_file_tree()"];
+        post_restore_cmds = ["lua open_file_tree()"];
+      };
+    };
     chadtree = {
       enable = true;
     };
@@ -72,13 +80,35 @@
       end
     end
 
-    local function open_file_tree()
-      cd_to_argv_dir()
-      vim.cmd "CHADopen --nofocus"
+    local function is_file_tree_open()
+      local buffers = vim.api.nvim_list_bufs()
+      for _, buffer in ipairs(buffers) do
+        if vim.fn.bufwinnr(buffer) > 0 and vim.api.nvim_buf_get_option(buffer, "filetype") == "CHADTree" then
+          return true
+        end
+      end
+      return false
     end
 
-    -- Open tree by default.
-    vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_file_tree })
+    function open_file_tree()
+      if not is_file_tree_open() then
+        chad.Open("--nofocus")
+      end
+    end
+
+    local function init_file_tree()
+      cd_to_argv_dir()
+      open_file_tree()
+    end
+
+    function close_file_tree()
+      local buffers = vim.api.nvim_list_bufs()
+      for _, buffer in ipairs(buffers) do
+        if vim.fn.bufwinnr(buffer) > 0 and vim.api.nvim_buf_get_option(buffer, "filetype") == "CHADTree" then
+          vim.api.nvim_buf_delete(buffer, { force = true })
+        end
+      end
+    end
 
     -- Set GUI font for Neovide/etc.
     -- Otherwise Neovide uses 16pt, which is too big.
@@ -93,6 +123,18 @@
       end,
       group = vim.api.nvim_create_augroup("lsp_format", { clear = true }),
       desc = "Format on Save",
+    })
+
+    -- Close if last window is CHADTree.
+    vim.o.confirm = true
+    vim.api.nvim_create_autocmd("BufEnter", {
+      group = vim.api.nvim_create_augroup("NvimTreeClose", {clear = true}),
+      callback = function()
+        local layout = vim.api.nvim_call_function("winlayout", {})
+        if layout[1] == "leaf" and vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(layout[2]), "filetype") == "CHADTree" and layout[3] == nil then
+          vim.cmd("quit")
+        end
+      end
     })
   '';
 }
