8c67ec018261e4884b69d69153be54d4a78c773b
John Chadwick <john@jchw.io>
2024-03-10 03:42:52 -0400

    Workaround for mimeapps not always being respected in Dolphin

--- modules/users/john/default.nix
+++ modules/users/john/default.nix
@@ -1,9 +1,12 @@
-{
+{ config, ... }: {
   imports = [
     ./librewolf.nix
   ];
   config = {
     home.stateVersion = "23.11";
     xdg.userDirs.enable = true;
+
+    # This shouldn't be necessary, but it works around KDE bugs.
+    home.file.".local/share/applications/mimeapps.list".source = config.lib.file.mkOutOfStoreSymlink "/etc/xdg/mimeapps.list";
   };
 }
