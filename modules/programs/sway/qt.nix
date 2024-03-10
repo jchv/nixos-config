5a717b556d6d00efc075e0341c6b4f7e115447ea
John Chadwick <john@jchw.io>
2024-03-09 22:46:34 -0500

    Disable Baloo under SwayWM

--- modules/programs/sway/qt.nix
+++ modules/programs/sway/qt.nix
@@ -15,7 +15,13 @@
     ];
 
     programs.sway.extraSessionCommands = ''
-        export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
+      export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
     '';
+
+    environment.etc."xdg/baloofilerc".source = (pkgs.formats.ini {}).generate "baloorc" {
+      "Basic Settings" = {
+        "Indexing-Enabled" = false;
+      };
+    };
   };
 }
