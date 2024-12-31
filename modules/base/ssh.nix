7255606e37bce3bf47aa49314062c8123ac2afc9
John Chadwick <john@jchw.io>
2024-12-31 18:46:02 -0500

    Update + clean up cruft

--- modules/base/ssh.nix
+++ modules/base/ssh.nix
@@ -36,33 +36,6 @@
     # Allow sudo using ssh-agent authentication
     security.pam.sshAgentAuth.enable = true;
 
-    # Fix for pam_ssh_agent_auth which is currently broken.
-    nixpkgs.overlays = [
-      (final: prev: {
-        pam_ssh_agent_auth = prev.pam_ssh_agent_auth.overrideAttrs (prevAttrs: {
-          nativeBuildInputs = (prevAttrs.nativeBuildInputs or [ ]) ++ [
-            final.autoreconfHook
-          ];
-          patches = (prevAttrs.patches or [ ]) ++ [
-            (final.fetchDebianPatch {
-              pname = "pam-ssh-agent-auth";
-              version = "0.10.3";
-              debianRevision = "11";
-              patch = "fix-configure.patch";
-              hash = "sha256-ymXv2o/NpFeVQ6r0hvJEeMpvs5Ht9jq4RSw8ssv43FY=";
-            })
-            (final.fetchDebianPatch {
-              pname = "pam-ssh-agent-auth";
-              version = "0.10.3";
-              debianRevision = "11";
-              patch = "1000-gcc-14.patch";
-              hash = "sha256-EvdaIhrfKZ1mB7qvNiGx/hYdthStgnhK7xvJEhhAFDQ=";
-            })
-          ];
-        });
-      })
-    ];
-
     # Necessary to be able to remotely use nixos-rebuild with agent auth.
     environment.variables.NIX_SSHOPTS = "-o ForwardAgent=yes";
   };
--- modules/base/tracing.nix
+++ modules/base/tracing.nix
@@ -1,20 +1,6 @@
 { config, pkgs, ... }:
 {
   config = {
-    boot.extraModulePackages = with config.boot.kernelPackages; [
-      (lttng-modules.overrideAttrs (
-        final: prev: {
-          version = "2.13.16";
-          src = pkgs.fetchFromGitHub {
-            owner = "lttng";
-            repo = "lttng-modules";
-            rev = "v2.13.16";
-            hash = "sha256-6SzKpBuKC98v2PbeyDoxEbt3i6KEztpOk0wlvP4NWBA=";
-          };
-        }
-      ))
-    ];
-
     environment.systemPackages = with pkgs; [
       babeltrace2
       lttng-tools
