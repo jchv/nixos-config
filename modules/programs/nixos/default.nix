b731f1c4ba570e86266066553476cccedecc0822
John Chadwick <john@jchw.io>
2025-05-31 21:02:50 -0400

    Update

--- modules/programs/nixos/default.nix
+++ modules/programs/nixos/default.nix
@@ -36,7 +36,6 @@
 
     programs.obs-studio = {
       enable = true;
-      enableVirtualCamera = true;
       plugins = [
         pkgs.obs-studio-plugins.wlrobs
         pkgs.obs-studio-plugins.obs-pipewire-audio-capture
