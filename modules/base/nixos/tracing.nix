3e2155dc54322560452d7007da611e550288e0f5
John Chadwick <john@jchw.io>
2025-06-28 22:36:36 -0400

    Tentative lttng-modules patch, doesn't work yet

--- modules/base/nixos/tracing.nix
+++ modules/base/nixos/tracing.nix
@@ -1,10 +1,24 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   config = {
     environment.systemPackages = [
       pkgs.babeltrace2
       pkgs.lttng-tools
     ];
+
+    # boot.extraModulePackages = with config.boot.kernelPackages; [
+    #   (lttng-modules.overrideAttrs (
+    #     final: prev: {
+    #       version = "2.14.0";
+    #       src = pkgs.fetchFromGitHub {
+    #         owner = "lttng";
+    #         repo = "lttng-modules";
+    #         rev = "v2.14.0";
+    #         hash = "sha256-70sCMyQA2Hfmimmv2Ea1f+zXGwms3MQ7xyTcOZfwT/A=";
+    #       };
+    #     }
+    #   ))
+    # ];
 
     systemd.services.lttng-sessiond = {
       description = "LTTng Session Daemon";
