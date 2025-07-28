bbc4a1fb989b839ea0dfbff45cea88707ecb0052
John Chadwick <john@jchw.io>
2025-07-28 16:12:52 -0400

    andou: enable linux-builder, disable sandbox

--- modules/machines/andou/default.nix
+++ modules/machines/andou/default.nix
@@ -1,6 +1,9 @@
+{ lib, ... }:
 {
   config = {
     networking.hostName = "andou";
     system.stateVersion = 5;
+    nix.linux-builder.enable = true;
+    nix.settings.sandbox = lib.mkForce false;
   };
 }
